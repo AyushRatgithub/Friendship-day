3b8cbc034d7bfd9dac987adbc466cca3e256995d
Ayush Ratnakar <161613430+AyushRatgithub@users.noreply.github.com>
2025-08-03 01:06:09 +0530

    Update PhotoBookScreen.jsx

--- src/components/PhotoBookScreen.jsx
+++ src/components/PhotoBookScreen.jsx
@@ -12,19 +12,19 @@ export default function PhotoBookScreen({ onNext }) {
     const photos = [
         {
             id: 1,
-            src: "/images/1.png",
+            src: "/images/1.jpg",
         },
         {
             id: 2,
-            src: "/images/2.png",
+            src: "/images/2.jpg",
         },
         {
             id: 3,
-            src: "/images/3.png",
+            src: "/images/3.jpg",
         },
         {
             id: 4,
-            src: "/images/4.png",
+            src: "/images/4.jpg",
         },
     ]
 
